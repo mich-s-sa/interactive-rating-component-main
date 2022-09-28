81b4abba732c06d24bb6cd64f6bea65958e901df
Mich Stacey <michlearnstocode@gmail.com>
2022-09-28 12:07:35 +0200

    design complete-Need to tidy code & show response.

---
+++ main.js
@@ -0,0 +1,8 @@
+let button = document.querySelector('.card__face__rating__submit');
+
+button.addEventListener('click', function(){
+  document.getElementById('card').classList.add('is-flipped');
+});
+
+
+
