a5b0a638b23c15218329447210785b6e32f47a3a
Mich Stacey <michlearnstocode@gmail.com>
2022-09-28 12:54:26 +0200

    Finished the challenge.

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let button = document.querySelector('.card__face__rating__submit');
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
   document.getElementById('card').classList.add('is-flipped');
+  let results = document.getElementsByTagName('input');
+  for (let i = 0; i < 5; i++) {
+    if (results[i].checked){
+      document.getElementById('answer').innerHTML = `${i+1}`
+      console.log(`put ${i+1} in the html code`)
+    }
+  }
 });
-
-
 
